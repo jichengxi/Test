b618b494b869a46c7b062a1ac53460a5f77db68b
jichengxi <948788684@qq.com>
2020-05-25 18:42:53 +0800

    xxxx

---
+++ Vue03/myvue/src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import config from '../config/index'
+import state from './state'
+import mutations from './mutations'
+import actions from './actions'
+import modules from './modules'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  modules
+})
