7512d11712b4567f1e843d201e7fcbf52c86924e
jichengxi <948788684@qq.com>
2020-05-28 18:17:37 +0800

    xxx

--- Vue03/myvue/vue.config.js
+++ Vue03/myvue/vue.config.js
@@ -5,10 +5,10 @@ const resolve = dir => path.join(__dirname, dir)
 const BASE_URL = process.env.NODE_ENV === 'production' ? '/iview-admin' : '/'
 
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ],
-  baseUrl: BASE_URL,
+  // presets: [
+  //   '@vue/cli-plugin-babel/preset'
+  // ],
+  publicPath: BASE_URL,
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src'))
