a67fc51da67ccdeeef7378e82b9068fd5cd497a6
jichengxi <948788684@qq.com>
2020-06-11 01:33:50 +0800

    xxx

--- Vue03/myvue/src/store/plugin/saveInLocal.js
+++ Vue03/myvue/src/store/plugin/saveInLocal.js
@@ -1,6 +1,7 @@
 export default store => {
   // 刷新浏览器后第一次定义的操作，可以写在这里
   // console.log('store初始化了')
+  // console.log(localStorage.state)
   if (localStorage.state) store.replaceState(JSON.parse(localStorage.state))
   store.subscribe((mutations, state) => {
     // console.log('提交mutations')
